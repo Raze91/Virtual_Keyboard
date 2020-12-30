cecc14a5deb168b06599d656ba282ba14d41835f
Sami Hattab <hattab_sami@yahoo.fr>
2020-12-30 11:01:14 +0100

    add min Azerty / Qwerty

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function () {
     const darkSwitch = document.querySelector("#darkSwitch");
+    const keyboardSwitch = document.querySelector("#keyboardSwitch");
     const cssLink = document.querySelector("#link");
     const buttons = document.querySelectorAll("input[type=button]");
     const textOutput = document.querySelector("#text-output");
@@ -7,9 +8,19 @@ window.addEventListener("load", function () {
     const capsCircle = document.querySelector(".capsCircle");
     const shift = document.querySelector("#shift");
     const shiftCircle = document.querySelector(".shiftCircle");
-    let specials = document.querySelectorAll(".special")
+    const specials = document.querySelectorAll(".special")
+    const letters = document.querySelectorAll(".letter");
     let isCapsLock = false;
     let isShift = false;
+    let isAzerty = true;
+
+    const minAzerty = ["²", "&", "é", '"', "'", "(", "-", "è", "_", "ç", "à", ")", "=", "Delete", "Tab", "a", "z", "e", "r", "t", "y", "u", "i", "o", "p", "^", "$", "q", "s", "d", "f", "g", "h", "j", "k", "l", "m", "ù", "*", "Enter", "<", "w", "x", "c", "v", "b", "n", ",", ";", ":", "!"];
+
+    const majAzerty = ["²", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "°", "+", "Delete", "Tab", "A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P", "¨", "£", "Q", "S", "D", "F", "G", "H", "J", "K", "L", "M", "%", "µ", "Enter", "<", "W", "X", "C", "V", "B", "N", "?", ".", "/", "§"];
+
+    const minQwerty = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Delete", "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
+
+    const majQwerty = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "Delete", "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", '"', "Enter", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?"]
 
     let majArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "°", "+", "¨", "£", "%", "µ", ">", "?", ".", "/", "§"];
     let minArray = ["&", "é", "\"", "'", "(", "-", "è", "_", "ç", "à", ")", "=", "^", "$", "ù", "*", "<", ",", ";", ":", "!"];
@@ -23,6 +34,21 @@ window.addEventListener("load", function () {
             cssLink.href = "css/keyboard-light.css";
         }
     });
+
+    // Qwerty / Azerty
+    keyboardSwitch.addEventListener("click", function () {
+        if (this.checked === true) {
+            isAzerty = true;
+            for (let i = 0; i < minAzerty.length; i++) {
+                buttons[i].value = minAzerty[i];
+            }
+        } else {
+            isAzerty = false;
+            for (let i = 0; i < minQwerty.length; i++) {
+                buttons[i].value = minQwerty[i];
+            }
+        }
+    })
 
     // Caps Lock
     capsLock.addEventListener("click", function () {
