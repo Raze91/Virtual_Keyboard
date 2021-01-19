2be3497f653b503cf0794520a6e3bca5cddcb3d5
Sami Hattab <hattab_sami@yahoo.fr>
2021-01-19 15:14:22 +0100

    changes some input into buttons in order to add icons

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ window.addEventListener("load", function () {
     const keyboardSwitch = document.querySelector("#keyboardSwitch");
     const cssLink = document.querySelector("#link");
     const buttons = document.querySelectorAll("input[type=button]");
+    const specialButtons = document.querySelectorAll("button");
     const textOutput = document.querySelector("#text-output");
     const capsLock = document.querySelector("#caps-lock");
     const capsCircle = document.querySelector(".capsCircle");
@@ -12,18 +13,18 @@ window.addEventListener("load", function () {
     let isShift = false;
     let isAzerty = true;
 
-    const minAzerty = ["²", "&", "é", '"', "'", "(", "-", "è", "_", "ç", "à", ")", "=", "Delete", "Tab", "a", "z", "e", "r", "t", "y", "u", "i", "o", "p", "^", "$", "q", "s", "d", "f", "g", "h", "j", "k", "l", "m", "ù", "*", "Enter", "<", "w", "x", "c", "v", "b", "n", ",", ";", ":", "!"];
+    const minAzerty = ["²", "&", "é", '"', "'", "(", "-", "è", "_", "ç", "à", ")", "=", "a", "z", "e", "r", "t", "y", "u", "i", "o", "p", "^", "$", "q", "s", "d", "f", "g", "h", "j", "k", "l", "m", "ù", "*", "<", "w", "x", "c", "v", "b", "n", ",", ";", ":", "!"];
 
-    const majAzerty = ["²", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "°", "+", "Delete", "Tab", "A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P", "¨", "£", "Q", "S", "D", "F", "G", "H", "J", "K", "L", "M", "%", "µ", "Enter", "<", "W", "X", "C", "V", "B", "N", "?", ".", "/", "§"];
+    const majAzerty = ["²", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "°", "+", "A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P", "¨", "£", "Q", "S", "D", "F", "G", "H", "J", "K", "L", "M", "%", "µ", "<", "W", "X", "C", "V", "B", "N", "?", ".", "/", "§"];
 
-    const minQwerty = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Delete", "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
+    const minQwerty = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
 
-    const majQwerty = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "Delete", "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", '"', "Enter", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?"]
+    const majQwerty = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", '"', "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?"]
 
     // Dark Mode 
     cssLink.href = "css/keyboard-dark.css";
     darkSwitch.addEventListener("click", function () {
-        this.checked ? cssLink.href = "css/keyboard-dark.css" : cssLink.href = "css/keyboard-light.css"; 
+        this.checked ? cssLink.href = "css/keyboard-dark.css" : cssLink.href = "css/keyboard-light.css";
     });
 
     // Qwerty / Azerty
@@ -41,17 +42,17 @@ window.addEventListener("load", function () {
         }
     })
 
-    // Caps Lock
-    capsLock.addEventListener("click", function () {
-        isCapsLock = !isCapsLock;
-        capsLocked(isCapsLock, capsCircle);
-    });
+    // // Caps Lock
+    // capsLock.addEventListener("click", function () {
+    //     isCapsLock = !isCapsLock;
+    //     capsLocked(isCapsLock, capsCircle);
+    // });
 
-    // Shift
-    shift.addEventListener("click", function () {
-        isShift = !isShift;
-        capsLocked(isShift, shiftCircle);
-    })
+    // // Shift
+    // shift.addEventListener("click", function () {
+    //     isShift = !isShift;
+    //     capsLocked(isShift, shiftCircle);
+    // })
 
     function capsLocked(boolean, circle) {
         if (boolean === true) {
@@ -86,28 +87,42 @@ window.addEventListener("load", function () {
     // onclick
     buttons.forEach(button =>
         button.addEventListener("click", function () {
-            switch (this.value) {
-                case "SPACE":
-                    textOutput.value += " ";
-                    break;
-                case "Delete":
-                    textOutput.value = textOutput.value.substring(0, textOutput.value.length - 1);
-                    break;
-                case "Enter":
-                    textOutput.value += "\n";
-                    break;
-                case "Tab":
-                    textOutput.value += "   "
-                    break;
-                default:
-                    if (isShift === true) {
-                        isShift = !isShift;
-                        textOutput.value += this.value;
-                        capsLocked(isShift, shiftCircle)
-                    } else {
-                        textOutput.value += this.value;
-                    }
+            if (isShift === true) {
+                isShift = !isShift;
+                textOutput.value += this.value;
+                capsLocked(isShift, shiftCircle)
+            } else {
+                textOutput.value += this.value;
             }
         })
+    )
+    specialButtons.forEach(button => {
+        console.log(button.id)
+        button.addEventListener("click", function () {
+            switch (this.id) {
+                case "caps-lock":
+                    isCapsLock = !isCapsLock;
+                    capsLocked(isCapsLock, capsCircle);
+                    break;
+                case "shift":
+                    isShift = !isShift;
+                    capsLocked(isShift, shiftCircle);
+                    break;
+                case "delete":
+                    textOutput.value = textOutput.value.substring(0, textOutput.value.length - 1);
+                    break;
+                case "space":
+                    textOutput.value += " ";
+                    break;
+                case "enter":
+                    textOutput.value += "\n";
+                    break;
+                case "tab":
+                    textOutput.value += "   "
+                    break;
+            }
+        })
+
+    }
     )
 })
